9c46914b9398c5433b7470215e0f16ebc60c36ff
benscott <ben@benscott.co.uk>
2015-08-28 16:37:31 +0100

    Removed DB setup to docker

--- provision/provision_app.sh
+++ provision/provision_app.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+DEV_MODE=0
+SYNCED_FOLDER=/vagrant
+PROVISION_FILE=/etc/app-provisioned
+PROVISION_COUNT=6 # Make sure to update this when adding new updates!
+PROVISION_STEP=0
 
 #
 # usage() function to display script usage
@@ -76,11 +81,10 @@ source "$PROVISION_FOLDER/settings.cfg"
 # Initial provision, step 1: install required packages
 #
 function provision_1(){
-
   # Install packages
   echo "Updating and installing packages"
   apt-get update
-  # We need the postgres clients to be able to connect to the PG docker
+  # We need the postgres client to connect to docker
   apt-get install -y python-dev python-pip python-virtualenv python-pastescript build-essential libpq-dev libxslt1-dev libxml2-dev git-core postgresql-client-common postgresql-client-8.4 libgeos-dev libldap2-dev libsasl2-dev libssl-dev
 }
 
@@ -107,10 +111,13 @@ function provision_2(){
   # Config
   echo "Creating config"
   mkdir -p /etc/ckan/default
-  CKAN_DB_URL="postgresql://$DB_USER:$DB_PASS@$DB_HOST/$DB_NAME"
-  DATASTORE_DB_URL="postgresql://$DB_USER:$DB_PASS@$DB_HOST/$DB_DATASTORE_NAME"
-  DATASTORE_DB_RO_URL="postgresql://$DB_RO_USER:$DB_PASS@$DB_HOST/$DB_DATASTORE_NAME"
-  cat "${PROVISION_FOLDER}/development.ini" | sed -e "s~%CKAN_DB_URL%~$CKAN_DB_URL~" -e "s~%CKAN_PACKAGER_HOST%~$CKAN_PACKAGER_HOST~" -e "s~%DATASTORE_DB_URL%~$DATASTORE_DB_URL~" -e "s~%SOLR_HOST%~$SOLR_HOST~" -e "s~%DATASTORE_DB_RO_URL%~$DATASTORE_DB_RO_URL~" > /etc/ckan/default/development.ini
+  export CKAN_DB_URL="postgresql://$DB_USER:$DB_PASS@$DB_HOST/$DB_NAME"
+  export DATASTORE_DB_URL="postgresql://$DB_USER:$DB_PASS@$DB_HOST/$DB_DATASTORE_NAME"
+  export DATASTORE_DB_RO_URL="postgresql://$DB_RO_USER:$DB_PASS@$DB_HOST/$DB_DATASTORE_NAME"
+
+  #  Replace variable placeholders in development.ini - need to be in environment
+  export SOLR_URL CKANPACKAGER_URL CKANPACKAGER_SECRET DATAPUSHER_URL CKANPACKAGER_URL CKANPACKAGER_SECRET GBIF_USERNAME GBIF_PASSWORD DOI_PASSWORD WINDSHAFT_HOST WINDSHAFT_PORT LDAP_URI LDAP_AUTH_DN LDAP_AUTH_PASS LDAP_BASE_DN GOOGLE_ANALYTICS
+  envsubst < "${PROVISION_FOLDER}/development.ini" > /etc/ckan/default/development.ini
 
   # Create virtual env
   echo "Creating virtual environment"
@@ -182,43 +189,18 @@ function provision_4(){
 }
 
 #
-# Initial provision, step 5: Install CKAN NHM extension and requirements.
-#
-function provision_5(){
-
-cat << EOF | PGPASSWORD=asdf psql -h $DB_HOST -U $PG_USER
-    -- Create the database user:
-    CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';
-    -- Create the database:
-    CREATE DATABASE $DB_NAME WITH OWNER $DB_USER;
-    -- Create the datastore:
-    CREATE USER $DB_RO_USER WITH PASSWORD '$DB_PASS';
-    CREATE DATABASE $DB_DATASTORE_NAME WITH OWNER $DB_USER;
-    -- CREATE EXTENSION citext
-    -- Create windshaft user
-    CREATE USER $DB_WINDSHAFT_USER WITH PASSWORD '$DB_PASS';
-    GRANT SELECT ON ALL TABLES IN SCHEMA public TO $DB_WINDSHAFT_USER;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO $DB_WINDSHAFT_USER;
-    -- Set per-user work mem
-    ALTER ROLE $DB_USER SET work_mem='4MB';
-    ALTER ROLE $DB_RO_USER SET work_mem='10MB';
-    ALTER ROLE $DB_WINDSHAFT_USER SET work_mem='100MB';
-EOF
-
-}
-
-
-#
 # Initial provision, step 6: Set up database
 #
-function provision_6(){
+function provision_5(){
   cd /usr/lib/ckan/default
   . /usr/lib/ckan/default/bin/activate
   echo "Init databases"
   paster --plugin=ckan db init -c /etc/ckan/default/development.ini
   paster --plugin=ckan user add admin email="$CKAN_ADMIN_EMAIL" password="$CKAN_ADMIN_PASS" -c /etc/ckan/default/development.ini
   paster --plugin=ckan sysadmin add admin -c /etc/ckan/default/development.ini
-  paster --plugin=ckan datastore set-permissions postgres -c /etc/ckan/default/development.ini
+
+  # Set permissions - piped to psql
+  paster --plugin=ckan datastore set-permissions -c /etc/ckan/default/development.ini | PGPASSWORD=$DB_ADMIN_PASS psql -h $DB_HOST -U $DB_ADMIN_USER
 
   # Init NHM database table - uses resource id foreign key so needs to come after the core ckan initdb
   cd /usr/lib/ckan/default/src/ckanext-nhm
@@ -249,7 +231,6 @@ elif [ "${PROVISION_VERSION}" -eq 0 ]; then
   provision_3
   provision_4
   provision_5
-  provision_6
   echo ${PROVISION_COUNT} > ${PROVISION_FILE}
 elif [ ${PROVISION_VERSION} -ge ${PROVISION_COUNT} ]; then
   echo "Server already provisioned"
